db5ed6e9bdef649294f100facb89f739d6a15441
huangwenlong <huang.wen.long@hotmail.com>
2021-07-02 21:17:27 +0800

    使用BackgroundWorker工作

--- AlgebraicExpressionSimplifier/MainWindow.xaml.cs
+++ AlgebraicExpressionSimplifier/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MathematicalExpressionCalculator
 {
@@ -34,34 +37,56 @@ namespace MathematicalExpressionCalculator
 
         private ObservableCollection<SymbolConstraint> constraints = new ObservableCollection<SymbolConstraint>();
 
+        private readonly BackgroundWorker bw = new BackgroundWorker();
         public MainWindow()
         {
             InitializeComponent();
             constraintListBox.ItemsSource = constraints;
+
+            bw.DoWork += Bw_DoWork;
+            bw.RunWorkerCompleted += Bw_RunWorkerCompleted;
+            bw.WorkerSupportsCancellation = true;
+        }
+
+        private void Bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            (string input, SymbolConstraint[] constraints) arg = ((string, SymbolConstraint[]))e.Argument;
+
+            ExpressionContext context = new ExpressionContext();
+            foreach (var item in arg.constraints)
+            {
+                context.SetSymbolAssignment(context.Symbol(item.name),
+                    ExpressionParser.Parse(item.value, context));
+            }
+            context.AnalyseConstraints();
+
+            IExpression ep = ExpressionParser.Parse(arg.input, context);
+            ep = ep.WithAssignment().Simplify();
+
+            string output = ep.ToString(), formula = ep.ToLaTeX();
+            e.Result = (output, formula);
+        }
+
+        private void Bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                OutputTextBlock.Text = "出错：" + e.Error.Message;
+            }
+            else if (e.Result != null)
+            {
+                (string output, string formula) result = ((string, string))e.Result;
+                OutputTextBlock.Text = result.output;
+                OutputFormula.Formula = result.formula;
+            }
+            GoButton.IsEnabled = true;
         }
 
         private void GoButton_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                ExpressionContext context = new ExpressionContext();
-                foreach (var item in constraints)
-                {
-                    context.SetSymbolAssignment(context.Symbol(item.name),
-                        ExpressionParser.Parse(item.value, context));
-                }
-                context.AnalyseConstraints();
-
-                IExpression ep = ExpressionParser.Parse(InputTextBox.Text, context);
-                ep = ep.WithAssignment().Simplify();
-                OutputTextBlock.Text = ep.ToString();
-                OutputFormula.Formula = ep.ToLaTeX();
-
-            }
-            catch (Exception exc)
-            {
-                OutputTextBlock.Text = exc.Message;
-            }
+            OutputTextBlock.Text = "计算中……";
+            GoButton.IsEnabled = false;
+            bw.RunWorkerAsync((InputTextBox.Text, constraints.ToArray()));
         }
 
         private void AddConstraintBtn_Click(object sender, RoutedEventArgs e)
@@ -116,19 +141,30 @@ namespace MathematicalExpressionCalculator
 
         private void InputTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            try
+            var input = InputTextBox.Text;
+            ThreadPool.QueueUserWorkItem(_ =>
             {
-                ExpressionContext context = new ExpressionContext();
-                IExpression ep = ExpressionParser.Parse(InputTextBox.Text, context);
-                InputFormula.Formula = ep.ToLaTeX();
-                InputTextBox.FontWeight = FontWeights.Normal;
-                InputTextBox.Foreground = Brushes.Black;
-            }
-            catch
-            {
-                InputTextBox.FontWeight = FontWeights.Heavy;
-                InputTextBox.Foreground = Brushes.OrangeRed;
-            }
+                try
+                {
+                    ExpressionContext context = new ExpressionContext();
+                    IExpression ep = ExpressionParser.Parse(input, context);
+                    string formula = ep.ToLaTeX();
+                    this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate ()
+                    {
+                        InputFormula.Formula = formula;
+                        InputTextBox.FontWeight = FontWeights.Normal;
+                        InputTextBox.Foreground = Brushes.Black;
+                    });
+                }
+                catch
+                {
+                    this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate ()
+                    {
+                        InputTextBox.FontWeight = FontWeights.Heavy;
+                        InputTextBox.Foreground = Brushes.OrangeRed;
+                    });
+                }
+            });
         }
 
         private void CopyButton_Click(object sender, RoutedEventArgs e)
